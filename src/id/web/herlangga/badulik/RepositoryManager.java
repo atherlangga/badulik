1524a24e28fd33371af17795e91e3d0827a7e08a
A. T. Herlangga <at.herlangga@gmail.com>
2009-09-22 06:11:12 +0700

    Add documentation for RepositoryManager.

--- src/id/web/herlangga/badulik/RepositoryManager.java
+++ src/id/web/herlangga/badulik/RepositoryManager.java
@@ -2,7 +2,34 @@ package id.web.herlangga.badulik;
 
 import id.web.herlangga.badulik.definition.Structure;
 
+/**
+ * Object repositories manager.
+ * 
+ * @author angga
+ * 
+ */
 public interface RepositoryManager {
+
+	/**
+	 * Get {@link ObjectRepository} given its name and the {@link Structure} of
+	 * the Domain Object kept.
+	 * 
+	 * @param name
+	 *            Unique identifier of the {@link ObjectRepository}.
+	 * @param objectStructure
+	 *            {@link Structure} of the Domain Object kept in the
+	 *            {@link ObjectRepository}.
+	 * @return Existing {@link ObjectRepository} with name and {@link Structure}
+	 *         specified, or new {@link ObjectRepository} based on name and
+	 *         {@link Structure} supplied.
+	 */
 	public ObjectRepository getRepository(String name, Structure objectStructure);
+
+	/**
+	 * Remove {@link ObjectRepository} on all of kept Domain Object.
+	 * 
+	 * @param name
+	 *            Unique identifier of the {@link ObjectRepository}.
+	 */
 	public void dropRepository(String name);
 }
