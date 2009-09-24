851d8846ffb55f256f90b1a1ae2d7398d8991d53
A. T. Herlangga <at.herlangga@gmail.com>
2009-09-24 09:42:16 +0700

    Change type internal value from integer to byte.

--- src/id/web/herlangga/badulik/definition/Type.java
+++ src/id/web/herlangga/badulik/definition/Type.java
@@ -9,35 +9,35 @@ import java.util.*;
  * 
  */
 public class Type {
-	private final int type;
+	private final byte type;
 
-	protected Type(int type) {
+	private Type(byte type) {
 		this.type = type;
 	}
 
-	public static Type of(int type) {
-		return (Type) typeMapping().get(new Integer(type));
+	public static Type of(byte type) {
+		return (Type) typeMapping().get(new Byte(type));
 	}
 
-	public int typeAsInteger() {
+	public byte typeAsByte() {
 		return type;
 	}
 	
 	private static final Hashtable typeMapping() {
 		Hashtable typeMapping = new Hashtable();
-		typeMapping.put(new Integer(1), INT);
-		typeMapping.put(new Integer(2), LONG);
-		typeMapping.put(new Integer(3), STRING);
-		typeMapping.put(new Integer(4), DATE);
-		typeMapping.put(new Integer(5), BOOL);
+		typeMapping.put(new Byte((byte) 1), INT);
+		typeMapping.put(new Byte((byte) 2), LONG);
+		typeMapping.put(new Byte((byte) 3), STRING);
+		typeMapping.put(new Byte((byte) 4), DATE);
+		typeMapping.put(new Byte((byte) 5), BOOL);
 		
 		return typeMapping;
 	}
 
-	public static final Type INT = new Type(1);
-	public static final Type LONG = new Type(2);
-	public static final Type STRING = new Type(3);
-	public static final Type DATE = new Type(4);
-	public static final Type BOOL = new Type(5);
+	public static final Type INT = new Type((byte) 1);
+	public static final Type LONG = new Type((byte) 2);
+	public static final Type STRING = new Type((byte) 3);
+	public static final Type DATE = new Type((byte) 4);
+	public static final Type BOOL = new Type((byte) 5);
 
 }
